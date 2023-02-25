4e2f537fc278807c8567d4a7f25e2933a9b0f5aa
Imam Setiawan <s.imam29@gmail.com>
2023-02-25 18:02:17 +0700

    func overloading challenge - solution pt 1

--- challenge-1/interfaces.ts
+++ challenge-1/interfaces.ts
@@ -1,23 +1,36 @@
-interface House {
+import houses from "./houses";
 
+interface House {
+    name: string;
+    planets: string | string[]
 }
 
 interface HouseWithID {
-
+    id: number,
+    name: string;
+    planets: string | string[]
 }
 
-function findHouses(houses: string): HouseWithID;
+function findHouses(houses: string): HouseWithID[];
+
 function findHouses(
     houses: string,
     filter: (house: House) => boolean
-): HouseWithID;
-function findHouses(houses: House[]): HouseWithID;
+): HouseWithID[];
+
+function findHouses(houses: House[]): HouseWithID[];
+
 function findHouses(
     houses: House[],
     filter: (house: House) => boolean
-): HouseWithID;
+): HouseWithID[];
 
-let houses = require('./houses.json');
+function findHouses(
+    houses: string | House[],
+    filter?: (house: House) => boolean
+): HouseWithID[] {
+    return []
+};
 
 console.log(
     findHouses(JSON.stringify(houses), ({name}) => name === "Atreides")
