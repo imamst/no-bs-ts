146fb384d0bdeffce33e5eccc6943159f1c2bc7f
Imam Setiawan <s.imam29@gmail.com>
2023-02-26 21:43:30 +0700

    No BS TS - Challenge #1

--- challenge-1/interfaces.ts
+++ challenge-1/interfaces.ts
@@ -5,35 +5,24 @@ interface House {
     planets: string | string[]
 }
 
-interface HouseWithID {
-    id: number,
-    name: string;
-    planets: string | string[]
+interface HouseWithID extends House {
+    id: number;
 }
 
-function findHouses(houses: string): HouseWithID[];
-
 function findHouses(
-    houses: string,
-    filter: (house: House) => boolean
-): HouseWithID[];
-
-function findHouses(houses: House[]): HouseWithID[];
-
-function findHouses(
-    houses: House[],
-    filter: (house: House) => boolean
-): HouseWithID[];
-
-function findHouses(
-    houses: string | House[],
+    input: string | House[],
     filter?: (house: House) => boolean
 ): HouseWithID[] {
-    return []
+    const houses: House[] = typeof input === "string" ? JSON.parse(input) : input;
+
+    return (filter ? houses.filter(filter) : houses).map((house) => ({
+        id: houses.indexOf(house),
+        ...house,
+    }));
 };
 
 console.log(
     findHouses(JSON.stringify(houses), ({name}) => name === "Atreides")
 );
 
-console.log(findHouses(houses, ({name}) => name === "Harkomen"))
+console.log(findHouses(houses, ({name}) => name === "Harkonnen"))
