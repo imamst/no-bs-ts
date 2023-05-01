3d77e73f9f67b945273acb1101352114aeb88c3a
Imam Setiawan <s.imam29@gmail.com>
2023-05-01 14:11:27 +0700

    No BS TS - Challenge 3 (Basic Solution)

--- challenge-3/basic-event-handler.ts
+++ challenge-3/basic-event-handler.ts
@@ -1,21 +1,56 @@
-class EventProcessor {
-    handleEvent(eventName: ..., data: ...): void {
+type FilterFunction<T> = (data: T[keyof T]) => boolean;
+type Filters<T> = Record<keyof T, FilterFunction<T>[]>;
 
+type MapFunction<T> = (data: T[keyof T]) => T[keyof T];
+type Maps<T> = Record<keyof T, MapFunction<T>[]>;
+
+type ProcessedEvent<T> = {
+    eventName: keyof T,
+    data: T[keyof T]
+}
+
+class EventProcessor<T extends {}> {
+    private filters: Filters<T> = <Filters<T>>{};
+    private maps: Maps<T> = <Maps<T>>{};
+    private processed: ProcessedEvent<T>[] = [];
+
+    handleEvent<K extends keyof T>(eventName: K, data: T[K]): void {
+        let allowEvent = true;
+        for (const filter of this.filters[eventName] ?? []) {
+            if (!filter(data)) {
+                allowEvent = false;
+                break;
+            }
+        }
+
+        if (allowEvent) {
+            let mappedData = {...data};
+            for (const map of this.maps[eventName] ?? []) {
+                mappedData = <T[K]>map(mappedData);
+            }
+
+            this.processed.push({
+                eventName,
+                data: mappedData,
+            });
+        }
     }
 
-    addFilter(
-        eventName: ...,
-        filter: (data: ...) => boolean
+    addFilter<K extends keyof T>(
+        eventName: K,
+        filter: (data: T[K]) => boolean
     ): void {
-
+        this.filters[<keyof T>eventName] ||= [];
+        this.filters[<keyof T>eventName].push(filter as FilterFunction<T>);
     }
 
-    addMap(eventName: ..., map: (data: ...) => ...): void {
-
+    addMap<K extends keyof T>(eventName: K, map: (data: T[K]) => T[K]): void {
+        this.maps[<keyof T>eventName] ||= [];
+        this.maps[<keyof T>eventName].push(map as unknown as MapFunction<T>);
     }
 
     getProcessedEvents() {
-
+        return this.processed;
     }
 }
 
@@ -36,7 +71,6 @@ uep.addMap("login", (data) => ({
 }));
 
 uep.handleEvent("login", {
-    user: null,
     name: "jack"
 });
 
